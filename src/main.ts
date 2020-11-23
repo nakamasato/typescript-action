dd0f6735c2e6af47dcf9d451115cf2a2eaae9b50
Naka Masato <masatonaka1989@gmail.com>
2020-11-23 17:31:26 +0900

    Enable to get `base` & `head` (#1)
    Co-authored-by: Naka Masato <nakamasato1989@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import { context } from '@actions/github'
+import { wait } from './wait'
 
 async function run(): Promise<void> {
   try {
@@ -11,6 +12,30 @@ async function run(): Promise<void> {
     core.debug(new Date().toTimeString())
 
     core.setOutput('time', new Date().toTimeString())
+
+    const eventName = context.eventName
+    console.log(eventName)
+    let base: string | undefined
+    let head: string | undefined
+
+    switch (eventName) {
+      case 'pull_request':
+        base = context.payload.pull_request?.base?.sha
+        head = context.payload.pull_request?.head?.sha
+        break
+      case 'push':
+        base = context.payload.before
+        head = context.payload.after
+        break
+      default:
+        core.setFailed(
+          `This action only supports pull requests and pushes, ${context.eventName} events are not supported. ` +
+          "Please submit an issue on this action's GitHub repo if you believe this in correct."
+        )
+    }
+    console.log(`base: ${base}, head: ${head}`);
+
+
   } catch (error) {
     core.setFailed(error.message)
   }
